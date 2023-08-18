fa2a507b79a6c1e70d9f427c7d55daf301711c88
Linyh1999 <101108289+Linyh1999@users.noreply.github.com>
2023-08-18 15:26:36 +0800

    Update swin.py

--- models/swin.py
+++ models/swin.py
@@ -12,6 +12,7 @@ from timm.models.layers import DropPath, to_2tuple, trunc_normal_
 import torch.nn.functional as F
 
 
+
 class Mlp(nn.Module):
     def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
         super().__init__()
